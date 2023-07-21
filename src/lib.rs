7ceb0f2df192776bfa61b7785c800bc2f5618694
yuk1ty <yuk1ty@users.noreply.github.com>
2023-07-22 00:48:02 +0900

    not to use DefaultOptions directly

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use anyhow::Result;
-use bincode::{
-    config::{BigEndian, WithOtherEndian},
-    DefaultOptions, Options,
-};
+use bincode::Options;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 
@@ -51,26 +48,28 @@ pub fn build_query(domain_name: &str, record_type: RecordType) -> Result<Vec<u8>
         class: Class::In as u16,
     };
 
-    let config = DefaultOptions::new().with_big_endian();
-
-    let mut bytes = header_to_bytes(header, config)?;
-    bytes.extend_from_slice(&question_to_bytes(question, config)?);
+    let mut bytes = header_to_bytes(header)?;
+    println!("bytes = {:02x?}", bytes);
+    bytes.extend_from_slice(&question_to_bytes(question)?);
+    println!("concat = {:02x?}", bytes);
 
     Ok(bytes)
 }
 
-fn header_to_bytes(
-    header: DNSHeader,
-    config: WithOtherEndian<DefaultOptions, BigEndian>,
-) -> Result<Vec<u8>> {
-    config.serialize(&header).map_err(|e| anyhow::anyhow!(e))
+fn header_to_bytes(header: DNSHeader) -> Result<Vec<u8>> {
+    bincode::options()
+        .with_big_endian()
+        .serialize(&header)
+        .map(|v| v[1..].to_vec())
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
-fn question_to_bytes(
-    question: DNSQuestion,
-    config: WithOtherEndian<DefaultOptions, BigEndian>,
-) -> Result<Vec<u8>> {
-    config.serialize(&question).map_err(|e| anyhow::anyhow!(e))
+fn question_to_bytes(question: DNSQuestion) -> Result<Vec<u8>> {
+    bincode::options()
+        .with_big_endian()
+        .serialize(&question)
+        .map(|v| v[1..].to_vec())
+        .map_err(|e| anyhow::anyhow!(e))
 }
 
 fn encode_dns_name(name: &str) -> Result<Vec<u8>> {
@@ -85,6 +84,8 @@ fn encode_dns_name(name: &str) -> Result<Vec<u8>> {
 
 #[cfg(test)]
 mod tests {
+    use std::net::{Ipv4Addr, UdpSocket};
+
     use crate::{build_query, encode_dns_name, RecordType};
 
     #[test]
@@ -96,6 +97,25 @@ mod tests {
     #[test]
     fn test_build_query() {
         let query = build_query("www.example.com", RecordType::A).unwrap();
-        println!("{:x?}", query);
+        println!("query = {:02x?}", query);
+    }
+
+    #[test]
+    fn send_udp_request_to_google_dns_server_and_get_the_response() {
+        let query = build_query("www.example.com", RecordType::A).unwrap();
+
+        let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).unwrap();
+
+        // Send our query to 8.8.8.8, port 53. Port 53 is the DNS port.
+        socket.send_to(&query, ("8.8.8.8", 53)).unwrap();
+
+        // Read the response. UDP DNS responses are usually less than 512 bytes
+        // (see https://www.netmeister.org/blog/dns-size.html for MUCH more on that)
+        // so reading 1024 bytes is enough
+        let mut response = [0; 1024];
+        let (_, _) = socket.recv_from(&mut response).unwrap();
+
+        // Process the response as needed
+        println!("Response: {:?}", &response);
     }
 }
