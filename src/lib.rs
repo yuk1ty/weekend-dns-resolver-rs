b927e381c98d59e9dbfa596d6088677bdf2a0bf7
yuk1ty <yuk1ty@users.noreply.github.com>
2023-07-27 23:58:23 +0900

    parse DNS question

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use rand::Rng;
 pub mod consts {
     pub const DNS_BUF_SIZE: usize = 1024;
     pub const HEADER_SIZE: usize = 12;
+    pub const QUESTION_DATA_SIZE: usize = 4;
 }
 
 #[derive(Debug, Default, DekuWrite)]
@@ -48,6 +49,54 @@ pub struct DNSQuestion {
     pub name: Vec<u8>,
     pub kind: u16,
     pub class: u16,
+}
+
+impl TryFrom<(Vec<u8>, &[u8])> for DNSQuestion {
+    type Error = anyhow::Error;
+
+    fn try_from((name, data): (Vec<u8>, &[u8])) -> std::result::Result<Self, Self::Error> {
+        Ok(DNSQuestion {
+            name,
+            kind: u16::from_be_bytes(data[0..2].try_into()?),
+            class: u16::from_be_bytes(data[2..4].try_into()?),
+        })
+    }
+}
+
+pub fn parse_question(reader: &mut Cursor<&[u8; consts::DNS_BUF_SIZE]>) -> Result<DNSQuestion> {
+    let name = decode_name(reader)?;
+    let data = &mut [0; consts::QUESTION_DATA_SIZE];
+    reader.read_exact(data)?;
+    let data: &[u8] = data;
+    DNSQuestion::try_from((name.into(), data))
+}
+
+fn decode_name(reader: &mut Cursor<&[u8; consts::DNS_BUF_SIZE]>) -> Result<String> {
+    let mut cursor = reader.position();
+    let mut parts = Vec::new();
+    let mut length = reader.get_ref()[cursor as usize];
+
+    while length != 0 {
+        if (length & 0b11000000) != 0 {
+            parts.push(decode_compressed_name(reader));
+            cursor += 2;
+            reader.set_position(cursor);
+            return Ok(parts.join("."));
+        } else {
+            let (start, end) = ((cursor + 1) as usize, (cursor + length as u64 + 1) as usize);
+            parts.push(String::from_utf8(reader.get_ref()[start..end].to_vec())?);
+            cursor += length as u64 + 1;
+            length = reader.get_ref()[cursor as usize];
+        }
+    }
+
+    cursor += 1;
+    reader.set_position(cursor);
+    Ok(parts.join("."))
+}
+
+fn decode_compressed_name(reader: &mut Cursor<&[u8; consts::DNS_BUF_SIZE]>) -> String {
+    todo!()
 }
 
 #[repr(u16)]
@@ -110,7 +159,8 @@ mod tests {
     };
 
     use crate::{
-        build_query, consts, encode_dns_name, header_to_bytes, parse_header, DNSHeader, RecordType,
+        build_query, consts, encode_dns_name, header_to_bytes, parse_header, parse_question,
+        DNSHeader, RecordType,
     };
     use deku::prelude::*;
 
@@ -172,6 +222,35 @@ mod tests {
 
         let mut cur = Cursor::new(&response);
         println!("{:?}", parse_header(&mut cur));
-        println!("{:?}", cur.position());
+        println!("header pos = {:?}", cur.position());
+        println!("{:?}", parse_question(&mut cur));
+        println!("question pos = {:?}", cur.position());
+    }
+
+    #[test]
+    fn test_decode_name() {
+        let mut buf = [0; consts::DNS_BUF_SIZE];
+        buf[0] = 3;
+        buf[1] = 'w' as u8;
+        buf[2] = 'w' as u8;
+        buf[3] = 'w' as u8;
+        buf[4] = 7;
+        buf[5] = 'e' as u8;
+        buf[6] = 'x' as u8;
+        buf[7] = 'a' as u8;
+        buf[8] = 'm' as u8;
+        buf[9] = 'p' as u8;
+        buf[10] = 'l' as u8;
+        buf[11] = 'e' as u8;
+        buf[12] = 3;
+        buf[13] = 'c' as u8;
+        buf[14] = 'o' as u8;
+        buf[15] = 'm' as u8;
+        buf[16] = 0;
+
+        let mut cur = Cursor::new(&buf);
+        let name = super::decode_name(&mut cur);
+        assert_eq!(name.unwrap(), "www.example.com");
+        assert_eq!(cur.position(), 17);
     }
 }
